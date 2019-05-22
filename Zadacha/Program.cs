0b9f2ddf75a61913e18cff7285764104cfd09d45
BuildTools <unconfigured@null.spigotmc.org>
2019-05-22 17:49:55 +0300

    initial commit

---
+++ Zadacha/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //106/2
+            Console.WriteLine("Total elements ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Searched number ");
+            int searchnum = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int index1 = 0;
+            int index2 = n - 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[index1] + array[index2] > searchnum)
+                    index2--;
+                else if (array[index1] + array[index2] < searchnum)
+                    index1++;
+                else if (index1 != index2)
+                {
+                    Console.WriteLine($"numbers {array[index1]} and {array[index2]} equals {searchnum}");
+                    return;
+                }
+            }
+            Console.WriteLine($"there is not number equal {searchnum}");
+        }
+    }
+    }
+
